fcfbee9aec7b7fcaffa6f3aba5d88a1bd08719c1
JessicaGarson <jessica.garson@gmail.com>
2018-05-07 16:53:21 -0400

    cleaning up

--- sonic_pi_songs/sample_song.rb
+++ sonic_pi_songs/sample_song.rb
@@ -1,8 +1,3 @@
-live_loop :my_voice do
-  sample "/Users/jessicagarson/Documents/recordings/no.wav", beat_stretch: 3, rate: 0.5
-  sleep 5
-end
-
 live_loop :lets_start do
   synth :pretty_bell, attack: 2
   sleep 0.25
@@ -21,5 +16,17 @@ live_loop :drums do
   else
     sample :drum_tom_mid_hard
     sleep 0.7
+  end
+end
+
+live_loop :my_voice do
+  sample "/Users/jessicagarson/Documents/recordings/no.wav", beat_stretch: 3, rate: 0.5
+  sleep 5
+end
+
+live_loop :distort_all_the_things do
+  with_fx :distortion do
+    synth :zawa
+    sleep 0.4
   end
 end
